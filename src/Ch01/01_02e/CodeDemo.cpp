1492707d0448557e3e4b8e659743032260b22e61
IceCreamSandwhich <161404101+IceCreamSandwhich@users.noreply.github.com>
2025-06-04 12:48:40 +0000

    testing a block of code

--- src/Ch01/01_02e/CodeDemo.cpp
+++ src/Ch01/01_02e/CodeDemo.cpp
@@ -2,11 +2,21 @@
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
 
+// #include <iostream>
+
+// int main(){
+//     std::cout << "Hi There!" << std::endl;
+
+//     std::cout << std::endl << std::endl;
+//     return 0;
+// }
+
 #include <iostream>
 
 int main(){
-    std::cout << "Hi There!" << std::endl;
-
-    std::cout << std::endl << std::endl;
+    int age;
+    std::cout << "Enter your age: ";
+    std::cin >> age;
+    std::cout << "You are " << age << " years old." << std::endl << std::endl;
     return 0;
 }
