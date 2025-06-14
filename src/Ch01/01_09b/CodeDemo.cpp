78352a5e6e70efbacfa5e2588fe654fd9d499e44
IceCreamSandwhich <161404101+IceCreamSandwhich@users.noreply.github.com>
2025-06-14 17:05:51 +0000

    Challenge 1_9

--- src/Ch01/01_09b/CodeDemo.cpp
+++ src/Ch01/01_09b/CodeDemo.cpp
@@ -5,8 +5,10 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Hi There!" << std::endl;
+    std::string name;
+    std::cout <<"Enter your name:" << std::flush;
+    std::cin >> name;
 
-    std::cout << std::endl << std::endl;
+    std::cout << "Nice to meet you," << name <<"!" <<std::endl;
     return 0;
 }
